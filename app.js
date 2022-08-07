c5e8f74d6ea5d24fad937e2b475d4c4494a8cc52
Vero Strizinec <verostrizinec@gmail.com>
2022-08-07 16:47:48 -0300

    interactuando con html

--- app.js
+++ app.js
@@ -1,57 +1,53 @@
 // Proyecto JavaScript = TRITATO
 
-// Saludar al Usuario - 
-
-let nombreUsuario = prompt("Ingresa tu nombre")
-alert ("Bienvenido/a a Tritato" + " " + nombreUsuario)
-
 // INCORPORAR ARRAY
 // Lista de productos de Tritato
 
 const tritato = []
 
 class producto {
-    constructor(tipoDeProducto, precio, especialidad) {
+    constructor(id, tipoDeProducto, precio, especialidad) {
+        this.id = id;
         this.tipoDeProducto = tipoDeProducto;
         this.precio = precio;
         this.especialidad = especialidad;
     }
 }
 
-const producto1 = new producto ("Picada", 2750, "Comen 2 Pican 4")
+const producto1 = new producto (id= 1, "Picada", 2750, "Comen 2 Pican 4")
 tritato.push (producto1)
 
-const producto2 = new producto ("Picada 2", 5500, "Comen 4 Pican 8")
+const producto2 = new producto (id= 2, "Picada 2", 5500, "Comen 4 Pican 8")
 tritato.push (producto2)
 
-const producto3 = new producto ("Picada 3", 8200, "Comen 6 Pican 10")
+const producto3 = new producto (id= 3, "Picada 3", 8200, "Comen 6 Pican 10")
 tritato.push (producto3)
 
-const producto4 = new producto ("Brunch", 3500, "Sandwich de Lomo y Queso, en Pan de Masa Madre. Scones de Queso y Tomillo. Mini Budines de Naranja y Amapolas. Lingote de Chocotorta. Juego de Naranja exprimido. Infusiones")
+const producto4 = new producto (id= 4, "Brunch", 3500, "Sandwich de Lomo y Queso, en Pan de Masa Madre. Scones de Queso y Tomillo. Mini Budines de Naranja y Amapolas. Lingote de Chocotorta. Juego de Naranja exprimido. Infusiones")
 tritato.push (producto4)
 
-const producto5 = new producto ("Panificados", 850, "Focaccia Grande")
+const producto5 = new producto (id= 5, "Panificados", 850, "Focaccia Grande")
 tritato.push (producto5)
 
-const producto6 = new producto ("Panificados", 550, "Focaccia Chica")
+const producto6 = new producto (id= 6, "Panificados", 550, "Focaccia Chica")
 tritato.push (producto6)
 
-const producto7 = new producto ("Panificados", 650, "Chips Brioche (12 unidades)")
+const producto7 = new producto (id= 7, "Panificados", 650, "Chips Brioche (12 unidades)")
 tritato.push (producto7)
 
-const producto8 = new producto ("Panificados", 400, "Pan de Papa para Burguer (4 unidades)")
+const producto8 = new producto (id= 8, "Panificados", 400, "Pan de Papa para Burguer (4 unidades)")
 tritato.push (producto8)
 
-const producto9 = new producto ("Panificados", 850, "Pan de Masa Madre Grande")
+const producto9 = new producto (id= 9, "Panificados", 850, "Pan de Masa Madre Grande")
 tritato.push (producto9)
 
-const producto10 = new producto ("Panificados", 550, "Pan de Masa Madre Chico")
+const producto10 = new producto (id= 10, "Panificados", 550, "Pan de Masa Madre Chico")
 tritato.push (producto10)
 
-const producto11 = new producto ("Panificados", 500, "Pan Lacteado Artesano")
+const producto11 = new producto (id= 11, "Panificados", 500, "Pan Lacteado Artesano")
 tritato.push (producto11)
 
-const producto12 = new producto ("Panificados", 500, "Pan Lactal Integral")
+const producto12 = new producto (id= 12, "Panificados", 500, "Pan Lactal Integral")
 tritato.push (producto12)
 
 console.log(tritato)
@@ -79,7 +75,16 @@ function mostrar (sumaDeProductos) {
 sumar (500, 500)
 mostrar (resultado)
 
+// INTERACTUAR CON HTML aumentando precios
 
+let focacciaGrande = document.getElementsByClassName("focacciaGrande")
+
+focacciaGrande.innerText = "Focaccia Grande = $1000"
+console.log (focacciaGrande.innerText)
+
+let brunch = document.getElementById("brunch")
+brunch.innerText = "El precio del brunch aumentó a $3800"
+console.log (brunch.innerText)
 
 console.log('Fin del programa')
 
