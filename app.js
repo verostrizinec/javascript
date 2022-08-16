c7de1465c0952dc65744787f85ba9f118b1cd33e
Vero Strizinec <verostrizinec@gmail.com>
2022-08-16 18:58:44 -0300

    agreglando css

--- app.js
+++ app.js
@@ -14,41 +14,18 @@ class producto {
     }
 }
 
-const producto1 = new producto (id= 1, "Picada", 2750, "Comen 2 Pican 4")
-tritato.push (producto1)
-
-const producto2 = new producto (id= 2, "Picada 2", 5500, "Comen 4 Pican 8")
-tritato.push (producto2)
-
-const producto3 = new producto (id= 3, "Picada 3", 8200, "Comen 6 Pican 10")
-tritato.push (producto3)
-
-const producto4 = new producto (id= 4, "Brunch", 3500, "Sandwich de Lomo y Queso, en Pan de Masa Madre. Scones de Queso y Tomillo. Mini Budines de Naranja y Amapolas. Lingote de Chocotorta. Juego de Naranja exprimido. Infusiones")
-tritato.push (producto4)
-
-const producto5 = new producto (id= 5, "Panificados", 850, "Focaccia Grande")
-tritato.push (producto5)
-
-const producto6 = new producto (id= 6, "Panificados", 550, "Focaccia Chica")
-tritato.push (producto6)
-
-const producto7 = new producto (id= 7, "Panificados", 650, "Chips Brioche (12 unidades)")
-tritato.push (producto7)
-
-const producto8 = new producto (id= 8, "Panificados", 400, "Pan de Papa para Burguer (4 unidades)")
-tritato.push (producto8)
-
-const producto9 = new producto (id= 9, "Panificados", 850, "Pan de Masa Madre Grande")
-tritato.push (producto9)
-
-const producto10 = new producto (id= 10, "Panificados", 550, "Pan de Masa Madre Chico")
-tritato.push (producto10)
-
-const producto11 = new producto (id= 11, "Panificados", 500, "Pan Lacteado Artesano")
-tritato.push (producto11)
-
-const producto12 = new producto (id= 12, "Panificados", 500, "Pan Lactal Integral")
-tritato.push (producto12)
+tritato.push(new producto (1, "Picada", 2750, "Comen 2 Pican 4"))
+tritato.push(new producto (2, "Picada 2", 5500, "Comen 4 Pican 8"))
+tritato.push(new producto (3, "Picada 3", 8200, "Comen 6 Pican 10"))
+tritato.push(new producto (4, "Brunch", 3500, "Sandwich de Lomo y Queso, en Pan de Masa Madre. Scones de Queso y Tomillo. Mini Budines de Naranja y Amapolas. Lingote de Chocotorta. Juego de Naranja exprimido. Infusiones"))
+tritato.push(new producto (5, "Panificados", 850, "Focaccia Grande"))
+tritato.push(new producto (6, "Panificados", 550, "Focaccia Chica"))
+tritato.push(new producto (7, "Panificados", 650, "Chips Brioche (12 unidades)"))
+tritato.push(new producto (8, "Panificados", 400, "Pan de Papa para Burguer (4 unidades)"))
+tritato.push(new producto (9, "Panificados", 850, "Pan de Masa Madre Grande"))
+tritato.push(new producto (10, "Panificados", 550, "Pan de Masa Madre Chico"))
+tritato.push(new producto (11, "Panificados", 500, "Pan Lacteado Artesano"))
+tritato.push(new producto (12, "Panificados", 500, "Pan Lactal Integral"))
 
 console.log(tritato)
 
